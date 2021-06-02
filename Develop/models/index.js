db6af300353b784e0e8e07cdcb5ecef666cf88cc
Richo Ha <richard.dqh@gmail.com>
2021-06-02 22:10:33 +0800

    Filled out Index Model to relate all the models together

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_name',
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_name',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
